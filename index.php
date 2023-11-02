d3c6d24099588294dcb6f3919b699a7e48a74262
Sakinatou11 <safietou.guindo@groupe-esigelec.org>
2023-11-02 10:03:32 +0100

    page accueil

--- index.php
+++ index.php
@@ -1,11 +1,28 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nom de votre site</title>
+    <link rel="stylesheet" type="text/css" href="model.css">
+	<script>
+        document.addEventListener("DOMContentLoaded", function() {
+            setTimeout(function() {
+                document.getElementById("welcome-text").style.display = "block";
+            }, 2000); // Déclenche l'affichage après 2 secondes (2000 millisecondes)
+        });
+    </script>
+</head>
+<body>
+    <?php include("header.inc.php"); ?>
+    <?php include("menu.inc.php"); ?>
+    
+    <main>
+        <!-- Contenu de la page d'accueil -->
+		<h1 class="italique" id="lets-play">LET'S PLAY</h1>
+		<p id="welcome-text" style="display: none;">Ceci est la page d'accueil de notre site web.</p>
+    </main>
+    
+    <?php include("footer.inc.php"); ?>
+</body>
+</html>
