eb80e6e1deced7af11ec18aaf42f09aeb5c5d08f
Ousmane267 <148067315+Ousmane267@users.noreply.github.com>
2023-11-16 18:20:24 +0100

    contact.php
    Page de contact qui sera mise dans le menu.inc.php

---
+++ contact.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<?php
+    session_start();
+    include 'header.inc.php';
+    include 'menu.inc.php';
+    ?>
+<html>
+<head>
+   <title>Informations - Let'sPlay</title>
+</head>
+<body>
+   <?php
+
+      // Récupérer les informations de votre site
+      $nom = "Let'sPlay";
+   
+      $email = "support@letsplay.com";
+      $telephone = "+123456789";
+
+      // Afficher les informations sur la page
+      echo "<h1>Informations sur $nom</h1>";;
+      echo "<p>Let's play est un site de jeux gratuit en ligne  basé et créer à Rouen en 2023.</p>";
+      echo "<p>Il a été créer  afin de permettre aux membres de choisir les jeux de plateau </p>";
+      echo "<p>auxquels ils souhaitent jouer, ainsi que les dates où ils vont se rencontrer.</p>";
+
+      
+      include'footer.inc.php';
+   ?>
+</body>
+</html>
