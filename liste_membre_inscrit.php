da7971be134614f2b4dc39a7e30152a021290217
Ousmane267 <148067315+Ousmane267@users.noreply.github.com>
2023-11-20 21:48:12 +0100

    liste_membre_inscrit.php

--- liste_membre_inscrit.php
+++ liste_membre_inscrit.php
@@ -1,6 +1,6 @@
 <?php
-  include 'header.inc.php';
-  include 'menu.inc.php';
+    include 'header.inc.php';
+    include 'menu.inc.php';
 // Connexion à la base de données
 $servername = "localhost";
 $username = "root";
@@ -9,25 +9,31 @@ $dbname = "sophie";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
-    die("Connexion échouée: " . $conn->connect_error);
+    die("Erreur de connexion à la base de données : " . $conn->connect_error);
 }
 
-// Requête pour récupérer les membres inscrits à un jeu
-$sql = "SELECT * FROM partie_a_venir";
+// Récupérer les membres inscrits à la partie à venir
+$partie_id = 563; // ID de la partie à venir
+
+$sql = "SELECT * FROM partie_a_venir WHERE id_partie = $partie_id";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        // Affichage des informations des membres inscrits
-        echo "Nom du jeu: " . $row["nom_jeu"]. "<br>";
-        echo "Nom:  " . $row["nom_joueur"]. "<br>";
-        echo "Prénom:  " . $row["prenom_joueur"]. "<br>";
-        echo "Date de début: " . $row["date"]. "<br>";
-        echo "<br>";
+    // Afficher la liste des membres inscrits
+    echo "<h2>Liste des membres inscrits à la partie :</h2>";
+    echo "<ul>";
+    while ($row = $result->fetch_assoc()) {
+        echo "<p>"   . $row["nom_jeu"] . "<p>";
+        echo "<p>Nom : "  . $row["prenom_joueur"] . "<p>";
+        echo "<p>Prénom : "  . $row["nom_joueur"] . "<p>";
+        echo "<p>Début : "  . $row["date"] . "<p>";
     }
+    echo "</ul>";
 } else {
-    echo "Aucun membre inscrit trouvé.";
+    echo "Aucun membre inscrit à la partie.";
 }
+
+// Fermer la connexion à la base de données
 $conn->close();
-include 'footer.inc.php';
+include 'footer.inc.php'
 ?>
