6e8c1d1994c2dad8e4b04392d213777cfc4a18c8
Ousmane267 <148067315+Ousmane267@users.noreply.github.com>
2023-11-21 22:45:14 +0100

    Add files via upload
    Page affichant la liste des membres inscrits pour les différentes parties.

--- liste_membre_inscrit.php
+++ liste_membre_inscrit.php
@@ -1,39 +1,56 @@
 <?php
     include 'header.inc.php';
     include 'menu.inc.php';
-// Connexion à la base de données
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "sophie";
+// Connexion à la base de données MySQL
+$host = "localhost"; // Hôte de la base de données
+$dbname = "sophie"; // Nom de la base de données
+$username = "root"; // Nom d'utilisateur MySQL
+$password = ""; // Mot de passe MySQL
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Erreur de connexion à la base de données : " . $conn->connect_error);
+try {
+   $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+   die("Erreur de connexion à la base de données: " . $e->getMessage());
 }
 
-// Récupérer les membres inscrits à la partie à venir
-$partie_id = 563; // ID de la partie à venir
+// Récupération des parties à venir depuis la table partie_a_venir
+$query = "SELECT * FROM partie_a_venir";
+$stmt = $db->prepare($query);
+$stmt->execute();
+$parties = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-$sql = "SELECT * FROM partie_a_venir WHERE id_partie = $partie_id";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // Afficher la liste des membres inscrits
-    echo "<h2>Liste des membres inscrits à la partie :</h2>";
-    echo "<ul>";
-    while ($row = $result->fetch_assoc()) {
-        echo "<p>"   . $row["nom_jeu"] . "<p>";
-        echo "<p>Nom : "  . $row["prenom_joueur"] . "<p>";
-        echo "<p>Prénom : "  . $row["nom_joueur"] . "<p>";
-        echo "<p>Début : "  . $row["date"] . "<p>";
-    }
-    echo "</ul>";
-} else {
-    echo "Aucun membre inscrit à la partie.";
+// Affichage des parties à venir, des membres inscrits et de l'heure de début
+foreach ($parties as $partie) {
+   echo "Partie : " . $partie['nom_jeu'] . "<br>";
+   echo "Heure de début : " . $partie['heure_debut'] . "<br>";
+   echo "Membres inscrits :<br>";
+   
+   // Récupération des membres inscrits pour chaque partie
+   $query = "SELECT nom_joueur, prenom_joueur FROM partie_a_venir WHERE id_partie = ?";
+   $stmt = $db->prepare($query);
+   $stmt->execute([$partie['id_partie']]);
+   $membres = $stmt->fetchAll(PDO::FETCH_ASSOC);
+   
+   if (count($membres) > 0) {
+      foreach ($membres as $membre) {
+         echo $membre['nom_joueur'] . " " . $membre['prenom_joueur'] . "<br>";
+      }
+   } else {
+      echo "Aucun membre inscrit pour le moment.<br>";
+   }
+   
+   // Affichage du bouton d'inscription uniquement si le membre n'est pas déjà inscrit
+   $query = "SELECT COUNT(*) FROM partie_a_venir WHERE id_partie = ? AND nom_joueur = ? AND prenom_joueur = ?";
+   $stmt = $db->prepare($query);
+   $stmt->execute([$partie['id_partie'], "votre_nom", "votre_prénom"]);
+   $count = $stmt->fetchColumn();
+   
+   if ($count == 0) {
+      echo "<a href='inscription_partie_a_venir.php?id=" . $partie['id_partie'] . "'>S'inscrire</a><br><br>";
+   } else {
+      echo "Vous êtes déjà inscrit pour cette partie.<br><br>";
+   }
 }
-
-// Fermer la connexion à la base de données
-$conn->close();
-include 'footer.inc.php'
+include 'footer.inc.php';
 ?>
